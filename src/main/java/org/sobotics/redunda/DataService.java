f88f58689055d9014187c0f85db0a3ced9dfec9c
FelixSFD <felix.deil@student.uni-augsburg.de>
2017-03-21 12:26:48 +0100

    #7: + added method to push a file to Redunda

--- src/main/java/org/sobotics/redunda/DataService.java
+++ src/main/java/org/sobotics/redunda/DataService.java
@@ -1,11 +1,21 @@
 package org.sobotics.redunda;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Synchronizes files across bot-instances via Redunda
@@ -15,6 +25,11 @@ public class DataService {
 	 * The API-key for the current instance. You can get the key in the Instances-overview on Redunda
 	 * */
 	public String apiKey = "";
+	
+	/**
+	 * Stores if files should be uploaded automatically
+	 * */
+	private boolean autoUpload = false;
 	
 	/**
 	 * If `true`, `standby` will always return `false` for debugging purposes!
@@ -88,6 +103,53 @@ public class DataService {
 	 * */
 	public List<String> getTrackedFiles() {
 		return this.trackedFiles;
+	}
+	
+	/**
+	 * Uploads all tracked files to Redunda
+	 * */
+	public void pushFiles() {
+		
+	}
+	
+	/**
+	 * Uploads a file to Redunda
+	 * 
+	 * @param filename The name of the file to upload
+	 * @throws IOException if the file couldn't be read
+	 * */
+	public void pushFile(String filename) throws IOException {
+		String content = new String(Files.readAllBytes(Paths.get(filename)));
+		String encodedFilename = URLEncoder.encode(filename, "UTF-8");
+		
+		
+		String url = "https://redunda.sobotics.org/bots/data/"+encodedFilename+"?key="+this.apiKey;
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add request header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", "Redunda Library");
+		
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(content);
+		wr.flush();
+		wr.close();
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		String responseString = response.toString();
+		System.out.println(responseString);
 	}
 	
 	/**
